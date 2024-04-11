c4bd2631c114dd2ff1bbe48760cfa50d98ea3836
Programador7 <programador7@olan.edu.co>
2024-04-11 18:04:33 -0500

    Terminacion de la interfaz arreglos y solicitud a la api

--- src/index.js
+++ src/index.js
@@ -11,9 +11,43 @@ app.use(express.static("public"));
 
 
 app.get('/', (req, resp) =>{
-    resp.render("App");
+    resp.render("index");
 })
 
+app.post('/convert-mp3', async (req, resp) =>{
+   const VideoId = req.body.VideoID;
+   if(VideoId === undefined || VideoId === "" || VideoId === null){
+        return resp.render("index", {success: false, message: 'Por favor ingrese el Id del Video'});  
+   }else{
+
+        const url = `https://youtube-mp3-downloader2.p.rapidapi.com/?id=${VideoId}`
+        const option = {
+            "method": "GET",
+            "headers": { 
+                "x-rapidapi-key": process.env.API_KEY,
+                "x-rapidapi-host": process.env.API_HOST
+            }
+    };
+
+    try {
+        const FetchAPi = await fetch(url, option);
+        const ResponseFetch = await FetchAPi.json();
+
+        if(ResponseFetch.status === "ok"){
+            return resp.render("index", {success: true, song_title: ResponseFetch.title, song_link: ResponseFetch.link})
+        }
+
+    } catch (error) {
+        console.log("error", error);
+        return resp.render("index", { success: false, message: 'Error en la solicitud' });
+        
+    }
+    
+
+    
+
+}
+})
 
 app.listen(PORT, ()=>{
     console.log(`run port on ${PORT}`);
