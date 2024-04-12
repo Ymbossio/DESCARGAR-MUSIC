64b0b9ca10bd73ccab45f5afd758856a191fb498
Programador7 <programador7@olan.edu.co>
2024-04-12 11:08:10 -0500

    axios y correcion

--- src/index.js
+++ src/index.js
@@ -1,53 +1,44 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-PORT = process.env.PORT || 5665;
+const PORT = 5665;
 
-app.use(express.urlencoded({extended: true}));
-
-//set templace
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
-app.get('/', (req, resp) =>{
+app.get('/', (req, resp) => {
     resp.render("index");
-})
+});
 
-app.post('/convert-mp3', async (req, resp) =>{
-   const VideoId = req.body.VideoID;
-   if(VideoId === undefined || VideoId === "" || VideoId === null){
-        return resp.render("index", {success: false, message: 'Por favor ingrese el Id del Video'});  
-   }else{
+app.post('/convert-mp3', async (req, resp) => {
+    const VideoId = req.body.VideoID;
+    if (!VideoId) {
+        return resp.render("index", { success: false, message: 'Por favor ingrese el Id del Video' });
+    } else {
 
-        const url = `https://youtube-mp3-downloader2.p.rapidapi.com/?id=${VideoId}`
-        const option = {
-            "method": "GET",
-            "headers": { 
-                "x-rapidapi-key": process.env.API_KEY,
-                "x-rapidapi-host": process.env.API_HOST
-            }
-    };
-
-    try {
-        const FetchAPi = await fetch(url, option);
-        const ResponseFetch = await FetchAPi.json();
-
-        if(ResponseFetch.status === "ok"){
-            return resp.render("index", {success: true, song_title: ResponseFetch.title, song_link: ResponseFetch.link})
+        const options = {
+        method: 'GET',
+        url: 'https://youtube-mp3-downloader2.p.rapidapi.com/ytmp3/ytmp3/',
+        params: {
+            url: `https://www.youtube.com/watch?v=hN5MBlGv2Ac=${VideoId}`
+        },
+        headers: {
+            'X-RapidAPI-Key': '4025c81e2fmsh5f1db89d9893431p1ba301jsn00e17fdd250c',
+            'X-RapidAPI-Host': 'youtube-mp3-downloader2.p.rapidapi.com'
         }
+        };
 
-    } catch (error) {
-        console.log("error", error);
-        return resp.render("index", { success: false, message: 'Error en la solicitud' });    
+        try {
+            const response = await axios.request(options);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     }
-    
+});
 
-    
-
-}
-})
-
-app.listen(PORT, ()=>{
-    console.log(`run port on ${PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`Run on Port ${PORT}`);
+});
